8824ce674b9ac79b7fb93da6d574db55512e4381
umut <113436899+OkcuYazilimci@users.noreply.github.com>
2023-07-25 16:39:08 +0300

    www.42Ecole.fr

---
+++ ft_getnextline/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilsahin <ilsahin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/23 14:06:23 by ilsahin           #+#    #+#             */
+/*   Updated: 2023/07/23 15:54:30 by ilsahin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *line, char *buff);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*new_line(char *line);
+char	*ft_get_line(int fd, char *line);
+char	*ft_get_next_line(char	*line);
+
+#endif
---
+++ ft_getnextline/get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilsahin <ilsahin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/23 14:07:14 by ilsahin           #+#    #+#             */
+/*   Updated: 2023/07/23 15:35:40 by ilsahin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *line, char *buff);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*new_line(char *line);
+char	*ft_get_line(int fd, char *line);
+char	*ft_get_next_line(char	*line);
+
+#endif
